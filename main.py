c390cdcd518166c5f8cee09d008091f06026ee72
NightBuksich <logehyd@gmail.com>
2021-01-15 02:41:25 +0300

    animation added

--- main.py
+++ main.py
@@ -7,32 +7,41 @@ screen = pygame.display.set_mode(size)
 inJump = False
 gravity = 10
 delta = 10
-fps = 60
+fps = 16
+Mright = False
+Mleft = False
+animation = 0
 clock = pygame.time.Clock()
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    try:
-        image = pygame.image.load(fullname)
-    except pygame.error as e:
-        print('Не удается загрузить картинку:', name)
-        raise SystemExit(e)
-    image = image.convert_alpha()
-
-    if colorkey:
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    return image
-
+walking_on_right = [pygame.image.load("sprites/R_R1.png"), pygame.image.load("sprites/R_R2.png"),
+                    pygame.image.load("sprites/R_R3.png"), pygame.image.load("sprites/R_R4.png")]
+walking_on_left = [pygame.image.load("sprites/R_L1.png"), pygame.image.load("sprites/R_L2.png"),
+                   pygame.image.load("sprites/R_L3.png"), pygame.image.load("sprites/R_L4.png")]
+hero_stand = pygame.image.load('sprites/Stand.png')
 
 all_sprites = pygame.sprite.Group()
-hero_image = load_image('chu.png')
 hero = pygame.sprite.Sprite(all_sprites)
-hero.image = hero_image
+hero.image = hero_stand
 hero.rect = hero.image.get_rect()
 bg = pygame.image.load('data/bg.jpg')
 hero.rect.bottomright = 200, 400
+
+
+def draw():
+    global animation
+    screen.blit(bg, (0, 0))
+    if animation + 1 >= 16:
+        animation = 0
+    if Mleft:
+        screen.blit(walking_on_left[animation // 4], (hero.rect.left, hero.rect.top))
+        animation += 1
+    elif Mright:
+        screen.blit(walking_on_right[animation // 4], (hero.rect.left, hero.rect.top))
+        animation += 1
+    else:
+        screen.blit(hero_stand, (hero.rect.left, hero.rect.top))
+    pygame.display.flip()
+
+
 running = True
 
 while running:
@@ -43,8 +52,16 @@ while running:
             running = False
         if keys[pygame.K_a] and hero.rect.left > 0:
             hero.rect.left -= delta
-        if keys[pygame.K_d] and hero.rect.right < 707:
+            Mleft = True
+            Mright = False
+        elif keys[pygame.K_d] and hero.rect.right < 707:
             hero.rect.left += delta
+            Mleft = False
+            Mright = True
+        else:
+            Mleft = False
+            Mright = False
+            animation = 0
         if not inJump:
             if keys[pygame.K_SPACE]:
                 inJump = True
@@ -61,12 +78,8 @@ while running:
             inJump = False
             gravity = 10
 
-
     if hero.rect.bottom > 400:
         hero.rect.bottom = 400
-    screen.blit(bg, (0, 0))
-    all_sprites.draw(screen)
-    pygame.display.flip()
-
+    draw()
 
 pygame.quit()
